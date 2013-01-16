654d0a310a7b3c59a0dcdebfec5d5271152a21aa
Curtis Dutton <curtdutt@gmail.com>
2013-01-16 10:17:46 -0500

    guest-lvm-tape launcher was pointed to guest-lvm-backup.rkt.
    points guest-lvm-tape laucnher to guest-lvm-tape.rkt

--- info.rkt
+++ info.rkt
@@ -5,5 +5,5 @@
 (define name "guest-lvm-backup")
 (define compile-omit-paths '("test"))
 (define racket-launcher-names (list "guest-lvm-backup"  "guest-lvm-tape"))
-(define racket-launcher-libraries (list "guest-lvm-backup.rkt" "guest-lvm-backup.rkt"))
+(define racket-launcher-libraries (list "guest-lvm-backup.rkt" "guest-lvm-tape.rkt"))
 
