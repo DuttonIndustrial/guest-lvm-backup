0b856aa22073b433d02af07f049adc713f12f0de
Curtis Dutton <curtdutt@gmail.com>
2013-01-16 11:42:55 -0500

    Adds additional launchers to info.rkt

--- info.rkt
+++ info.rkt
@@ -4,6 +4,6 @@
 
 (define name "guest-lvm-backup")
 (define compile-omit-paths '("test"))
-(define racket-launcher-names (list "guest-lvm-backup"  "guest-lvm-tape"))
-(define racket-launcher-libraries (list "guest-lvm-backup.rkt" "guest-lvm-tape.rkt"))
+(define racket-launcher-names (list "guest-lvm-backup"  "guest-lvm-full-backup" "guest-lvm-locate" "guest-lvm-patch" "guest-lvm-signature" "guest-lvm-tape"))
+(define racket-launcher-libraries (list "guest-lvm-backup.rkt"  "guest-lvm-full-backup.rkt" "guest-lvm-locate.rkt" "guest-lvm-patch.rkt" "guest-lvm-signature.rkt" "guest-lvm-tape.rkt"))
 
