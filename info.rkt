e829a1e9d58e96ec2bc514ab5012d12291106238
Curtis Dutton <curtdutt@gmail.com>
2013-01-16 19:11:09 -0500

    Fixes a bad line in info.rkt.

--- info.rkt
+++ info.rkt
@@ -7,5 +7,4 @@
 (define racket-launcher-names (list "guest-lvm-backup"  "guest-lvm-full-backup" "guest-lvm-locate" "guest-lvm-patch" "guest-lvm-signature" "guest-lvm-tape"))
 (define racket-launcher-libraries (list "guest-lvm-backup.rkt"  "guest-lvm-full-backup.rkt" "guest-lvm-locate.rkt" "guest-lvm-patch.rkt" "guest-lvm-signature.rkt" "guest-lvm-tape.rkt"))
 (define install-collection "private/installer.rkt")
-(define post-install-cleanup
 (define launcher-link-destination "/usr/local/bin")
